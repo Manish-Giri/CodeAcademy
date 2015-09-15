dcc7cab71b1a63a01b8d7252efefe52a84969247
Manish-Giri <manish.giri.me@gmail.com>
2015-09-14 17:37:29 -0700

    #6 from Objects II

---
+++ Web-Development/Javascript/Objects II/6.js
@@ -0,0 +1,12 @@
+/*I.D., Please
+Alright! Let's get our hands dirty and start exploring some really cool stuff about objects in JavaScript. But before we can do that, how can we even tell if something is an object (as opposed to, say, a number or string)? It would be great if we could tell what type something is in JavaScript. Good thing there's a handy built-in operator to do this!
+
+Say we have a variable thing and we don't know what type thing is. We can call typeof thing to figure this out. Generally, the most useful types are "number," "string," "function," and of course, "object."
+
+As an example, the following example will print "object":
+
+var someObject = {someProperty: someValue};
+console.log( typeof someObject );
+Instructions
+In lines 3-6, we have an object, a number, and a string (in that order). Complete lines 7-9 so they will print the appropriate types of these variables.*/
+
