71cd42758bc53f117861b8bb40c0fed418c3c4bd
Manish Giri <manish.giri.me@gmail.com>
2015-10-10 19:12:13 -0400

    CSS for #3 from jQuery Effects

---
+++ Web-Development/jQuery/jQuery Effects/3.css
@@ -0,0 +1,16 @@
+body {
+    background-image: url('http://bit.ly/UpQgJ6');
+    repeat: no-repeat;
+}
+
+div {
+    height: 100px;
+    width: 100px;
+    border-radius: 100%;
+    background-color: #008800;
+    background-image: -webkit-gradient(linear, 0% 0%, 0% 100%, from(#003500), to(#008800));
+    background-image: -webkit-linear-gradient(left, #003500, #008800); 
+    background-image:    -moz-linear-gradient(left, #003500, #008800);
+    background-image:     -ms-linear-gradient(left, #003500, #008800);
+    background-image:      -o-linear-gradient(left, #003500, #008800);
+}
