10196e3a34fd0a1279bb2fe23500afca5a4caa66
Manish Giri <manish.giri.me@gmail.com>
2015-10-12 18:57:49 -0400

    #5 - print statements

---
+++ Web-Development/Ruby/1-Introduction/5.rb
@@ -0,0 +1,15 @@
+=begin'puts' and 'print'
+The print command just takes whatever you give it and prints it to the screen. puts (for "put string") is slightly different: it adds a new (blank) line after the thing you want it to print. You use them like this:
+
+puts "What's up?"
+print "Oxnard Montalvo"
+No parentheses or semicolons needed!
+
+Instructions
+In the editor, use at least one print statement and at least one puts statement. You can print out any strings you like! (Make sure to put your strings between quotes, like this: "Hello!".)
+=end
+
+print "Hello, World!"
+
+puts "Hello Ruby!"
+puts "Welcome!"
