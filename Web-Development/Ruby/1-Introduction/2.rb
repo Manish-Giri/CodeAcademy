af011ac941de296244ea3c3a28dfd18ad49ef589
Manish Giri <manish.giri.me@gmail.com>
2015-10-12 18:56:54 -0400

    #2 - Assigning value to variables

---
+++ Web-Development/Ruby/1-Introduction/2.rb
@@ -0,0 +1,20 @@
+
+Data Types: Numbers, Strings, Booleans
+In Ruby, your information (or data) can come in different types. There are three data types in Ruby that we're interested in right now: numbers, booleans (which can be true or false), and strings (words or phrases like "I'm learning Ruby!").
+
+Computer programs exist to quickly analyze and manipulate data. For that reason, it's important for us to understand the different data types that we can use in our programs.
+
+Reminder: never use quotation marks (' or ") with booleans, or Ruby will think you're talking about a string (a word or phrase) instead of a value that can be true or false. It's also important to remember that Ruby is case-sensitive (it cares about capitalization).
+
+Instructions
+Set the following variables to the corresponding values:
+
+my_num to the value 25
+my_boolean to the value true
+my_string to the value "Ruby" (note the capitalization!)
+
+my_num =  25   # Add your code here!
+
+my_boolean = true     # And here!
+
+my_string = "Ruby"    # Also here.
