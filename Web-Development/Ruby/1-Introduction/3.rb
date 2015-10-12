7480ca1d77f9ec3185a19af98985487ec3ec4e71
Manish Giri <manish.giri.me@gmail.com>
2015-10-12 18:57:26 -0400

    #3 - Assigning values and executing code

---
+++ Web-Development/Ruby/1-Introduction/3.rb
@@ -0,0 +1,10 @@
+Variables
+One of the most basic concepts in computer programming is the variable. You can think of a variable as a word or name that grasps a single value. For example, let's say you needed the number 25 from our last example, but you're not going to use it right away. You can set a variable, say my_num, to grasp the value 25 and hang onto it for later use, like this:
+
+my_num = 25
+Declaring variables in Ruby is easy: you just write out a name like my_num, use = to assign it a value, and you're done! If you need to change a variable, no sweat: just type it again and hit = to assign it a new value.
+
+Instructions
+Set the variable my_num to the value 100, then click the Run button to run your code.
+
+my_num = 100
