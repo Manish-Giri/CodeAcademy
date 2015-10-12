bd0fd728c0bacca55462685640ea927a256b79c0
Manish Giri <manish.giri.me@gmail.com>
2015-10-12 18:56:03 -0400

    1.rb - Ruby overview

---
+++ Web-Development/Ruby/1-Introduction/1.rb
@@ -0,0 +1,16 @@
+=begin
+Overview & Sneak Peek
+Ruby is a powerful, flexible programming language you can use in web/Internet development, to process text, to create games, and as part of the popular Ruby on Rails web framework. Ruby is:
+
+High-level, meaning reading and writing Ruby is really easy—it looks a lot like regular English!
+
+Interpreted, meaning you don't need a compiler to write and run Ruby. You can write it here at Codecademy or even on your own computer (many are shipped with the Ruby interpreter built in—we'll get to the interpreter later in this lesson).
+
+Object-oriented, meaning it allows users to manipulate data structures called objects in order to build and execute programs. We'll learn more about objects later, but for now, all you need to know is everything in Ruby is an object.
+
+Easy to use. Ruby was designed by Yukihiro Matsumoto (often just called "Matz") in 1995. Matz set out to design a language that emphasized human needs over those of the computer, which is why Ruby is so easy to pick up.
+
+This course assumes no previous knowledge of Ruby in particular or programming/computer science in general.
+
+Instructions
+Ready to learn Ruby? Click the Submit button to continue!=end
