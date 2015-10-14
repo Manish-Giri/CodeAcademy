67c4c4fe0b8c9f8a43668b0c49afdcee90898293
Manish-Giri <manish.giri.me@gmail.com>
2015-10-14 00:37:32 -0400

    #6 - object properties

---
+++ Web-Development/Ruby/1-Introduction/6.rb
@@ -0,0 +1,9 @@
+=begin
+Everything in Ruby is an Object
+Because everything in Ruby is an object (more on this later), everything in Ruby has certain built-in abilities called methods. You can think of methods as "skills" that certain objects have. For instance, strings (words or phrases) have built-in methods that can tell you the length of the string, reverse the string, and more.
+
+We also promised to tell you more about the interpreter. The interpreter is the program that takes the code you write and runs it. You type code in the editor, the interpreter reads your code, and it shows you the result of running your code in the console (the bottom window on the right).
+
+Instructions
+Enough small talk. Click Save & Submit Code to start learning about string methods!=end
+
