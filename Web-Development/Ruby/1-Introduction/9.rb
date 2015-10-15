92a66520091d9d86f3735bf9df2551e5f0331b0b
Manish Giri <manish.giri.me@gmail.com>
2015-10-14 21:15:30 -0400

    #9 - .upcase() and .downcase()

---
+++ Web-Development/Ruby/1-Introduction/9.rb
@@ -0,0 +1,15 @@
+=begin
+'.upcase' & '.downcase'
+Let's try one more method (er, two methods). As you might have guessed, the .upcase and .downcase methods convert a string to ALL UPPER CASE or all lower case, respectively.
+
+Instructions
+Call .upcase on your name to make your name ALL CAPS and use puts to print it to the screen, like this:
+
+puts "eric".upcase
+# ==> ERIC
+On the next line, call .downcase to make your name all lower case. Make sure to use puts so you can see it printed out!
+=end
+
+puts "manish".upcase
+
+puts "MANISH".downcase
