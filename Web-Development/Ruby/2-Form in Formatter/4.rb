685174bf4e9f50f6ee16d6a6372389fe6a712aa8
Manish-Giri <manish.giri.me@gmail.com>
2015-10-15 05:20:53 -0400

    #4 - more input with gets

---
+++ Web-Development/Ruby/2-Form in Formatter/4.rb
@@ -0,0 +1,19 @@
+# Repeat for More Input
+# All right! Now we need to repeat what we've done for last_name, city, and state.
+
+# Instructions
+# Add print prompts, variables, and gets.chomps for the user's last name, city, and state/province. Use last_name as the variable for the user's last name, city for their city, and state for their state or province. (Hint: prompt the user to provide an abbreviation for their state or province, such as "NY" for New York. This will naturally lead us to use .upcase later!)
+
+
+print "What's your first name?"
+
+first_name = gets.chomp
+
+print "What's your last name?"
+last_name = gets.chomp
+
+print "What's your city?"
+city = gets.chomp
+
+print "What's your state(Enter abbreviation)?"
+state = gets.chomp
