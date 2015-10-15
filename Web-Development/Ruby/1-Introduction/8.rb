155cda9bbdf44a1cdee75c0e1455972e68469787
Manish Giri <manish.giri.me@gmail.com>
2015-10-14 21:15:04 -0400

    #8 - .reverse()

---
+++ Web-Development/Ruby/1-Introduction/8.rb
@@ -0,0 +1,15 @@
+=begin
+The '.reverse' Method
+The .reverse method is called the same way .length is, but instead of asking Ruby to tell you how long a string is, it spits out a backwards version of the string you gave it. For instance,
+
+"Eric".reverse
+will result in
+
+"cirE"
+Reversing input can be useful if you want to sort a list of values from highest to lowest instead of lowest to highest. (We'll get to sorting in later lessons.)
+
+Instructions
+Call the .reverse method on your name. Don't forget those quotation marks around your name to make it a string!
+=end
+
+"Manish".reverse
