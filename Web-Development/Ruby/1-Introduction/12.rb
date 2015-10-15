353753169fcd4a963e8407dd4cc6584ed9aa6edf
Manish Giri <manish.giri.me@gmail.com>
2015-10-14 21:16:37 -0400

    #12 - Ruby naming conventions

---
+++ Web-Development/Ruby/1-Introduction/12.rb
@@ -0,0 +1,7 @@
+# Naming Conventions
+# There are many different kinds of variables you'll encounter as you progress through these courses, but right now we're just concerned with regular old local variables. By convention, these variables should start with a lowercase letter and words should be separated by underscores, like counter and masterful_method. Ruby won't stop you from starting your local variables with other symbols, such as capital letters, $s, or @s, but by convention these mean different things, so it's best to avoid confusion by doing what the Ruby community does.
+
+# Instructions
+# Create a variable name in the editor and set it equal to your name as a string (between quotes, like this: "Eric"). Your string can be capitalized, but name should be all lower case!
+
+name = "Manish"
