188b59c80cfb0bd4b87363c25f5b1956777c8765
Manish Giri <manish.giri.me@gmail.com>
2015-10-14 21:14:34 -0400

    #7 - string methods

---
+++ Web-Development/Ruby/1-Introduction/7.rb
@@ -0,0 +1,18 @@
+=begin
+The '.length' Method
+Methods are summoned using a .. If you have a string, "I love espresso", and take the .length of it, Ruby will return the length of the string (that is, the number of characters—letters, numbers, spaces, and symbols). Check it out:
+
+"I love espresso".length
+# ==> 15
+(That little line starting with the # isn't part of what you need to write—it just shows you the output Ruby will provide. More on this in the next section!)
+
+Taking the length of input can be useful if, for example, you want to make a website that takes credit card payments. Ruby can check to make sure the credit card number appears to be valid.
+
+Instructions
+Call the .length method on your name (remember to use quotes around your name).
+=end
+
+
+"Manish Giri".length
+
+#11
