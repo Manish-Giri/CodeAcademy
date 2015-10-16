ee39e5a47f42553cfa4fbae4813bec1b06cf2f0f
Manish-Giri <manish.giri.me@gmail.com>
2015-10-16 19:56:50 -0400

    #14 - using unless

---
+++ Web-Development/Ruby/3-Control Flow/15.rb
@@ -0,0 +1,9 @@
+# Unless
+# Good! Now let's review the unless statement.
+
+# problem = false
+# print "Good to go!" unless problem
+# Remember, this is basically a short hand if statement. It will do whatever you ask unless the condition is true. In our example, problem is false, so we don't have a problem. We print Good to go!
+
+# Instructions
+# Create an unless statement in the editor. The statement should print something to the console.
