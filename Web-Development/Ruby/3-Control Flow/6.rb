0c81d36e7cd0925a2cfe719cdd9fa6db48a600ef
Manish-Giri <manish.giri.me@gmail.com>
2015-10-16 19:55:49 -0400

    #6 - equality operator

---
+++ Web-Development/Ruby/3-Control Flow/6.rb
@@ -0,0 +1,16 @@
+# Equal or Not?
+# In Ruby, we assign values to variables using =, the assignment operator. But if we've already used = for assignment, how do we check to see if two things are equal? Well, we use ==, which is a comparator (also called a relational operator). == means "is equal to." When you type
+
+# x = 2
+# y = 2
+# if x == y
+#   print "x and y are equal!"
+# end
+# you're saying: "if x equals y, print 'x and y are equal!'" You can also check to see if two values are not equal using the != comparator.
+
+# Instructions
+# We've got two variables in the editor: is_true and is_false. Replace the __ with == or != to make is_true evaluate to true and is_false evaluate to false.
+
+is_true = 2 != 3
+
+is_false = 2 == 3
