5ffd1c1a0fe16a592c4eb5a99d04c5cb3fbd9d4c
Manish-Giri <manish.giri.me@gmail.com>
2015-10-16 19:56:23 -0400

    #14 - if, elsif and else

---
+++ Web-Development/Ruby/3-Control Flow/14.rb
@@ -0,0 +1,25 @@
+# If, Else, and Elsif
+# All right! You're all on your lonesome. (Well, not quite. We'll just leave this example here.)
+
+# a = 10
+# b = 11
+# if a < b
+#   print "a is less than b!"
+# elsif b < a
+#   print "b is less than a!"
+# else
+#   print "b is equal to a!"
+# end
+# Instructions
+# Create an if/else statement in the editor. Make sure to include at least one elsif. Each branch of the statement should print something to the console.
+
+a = 20
+b = 30
+
+if a < b
+    print "df"
+elsif b < a
+    print "sf"
+else
+    print "ss"
+end
