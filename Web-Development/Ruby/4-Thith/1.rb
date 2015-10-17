b8c624322631efc16aeb1a571382b963e110b47e
Manish Giri <manish.giri.me@gmail.com>
2015-10-17 15:34:44 -0400

    Project - replacing parts of input string

---
+++ Web-Development/Ruby/4-Thith/1.rb
@@ -0,0 +1,7 @@
+# What You'll Be Building
+# Now that we can direct our program using if / else statements, we can produce different results based on different user input.
+
+# In this project, we'll combine control flow with a few new Ruby string methods to Daffy Duckify a user's string, replacing each "s" with "th".
+
+# Instructions
+# Click Save & Submit Code to see the Daffy Duckifier in action and to start building your own!
