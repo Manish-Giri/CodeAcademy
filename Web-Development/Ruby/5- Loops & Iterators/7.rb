489d6256ecba5316b10aa898d0617c9525570a87
Manish Giri <manish.giri.me@gmail.com>
2015-10-18 22:15:00 -0400

    #7 - simple for with inclusive range

---
+++ Web-Development/Ruby/5- Loops & Iterators/7.rb
@@ -0,0 +1,9 @@
+# Building Your Own
+# Good work! You're ready to build your very own for loop.
+
+# Instructions
+# Write a for loop that puts the numbers 1 to 20, including 20, using either .. or .... Check Hint if you need a syntax refresher.
+
+for num in 1..20
+    puts num
+end
