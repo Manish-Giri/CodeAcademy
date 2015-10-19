7863d8fa3a2e474bf1a6fd8abc70aac01c4c4c90
Manish Giri <manish.giri.me@gmail.com>
2015-10-18 22:14:15 -0400

    #5 - for

---
+++ Web-Development/Ruby/5- Loops & Iterators/5.rb
@@ -0,0 +1,9 @@
+# The 'For' Loop
+# Sometimes you do know how many times you'll be looping, however, and when that's the case, you'll want to use a for loop.
+
+# Instructions
+# Take a look at the example for loop in the editor. Can you guess what it does? Click Save & Submit Code to see it in action.
+
+for num in 1...10
+  puts num
+end
