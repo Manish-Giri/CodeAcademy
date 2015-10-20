de7abf8464f8d0c9266129ae46bd62085395de54
Manish Giri <manish.giri.me@gmail.com>
2015-10-19 21:19:32 -0400

    #10 - introducing arrays

---
+++ Web-Development/Ruby/5- Loops & Iterators/10.rb
@@ -0,0 +1,9 @@
+# Saving Multiple Values
+# Let's say we want to save a range of numbers in a variable. How would we do this? A variable can only hold a single value, right?
+
+# In Ruby, we can pack multiple values into a single variable using an array. An array is just a list of items between square brackets, like so: [1, 2, 3, 4]. The items don't have to be in order—you can just as easily have [10, 31, 19, 400].
+
+# Instructions
+# Create a variable called my_array and set it equal to the numbers 1 through 5, inclusive, in order.
+
+my_array = [1,2,3,4,5]
