a440ba07133a569dcf85af7367e92eec6f6b9167
Manish Giri <manish.giri.me@gmail.com>
2015-10-19 21:22:43 -0400

    #15 - rewriting a while using until

---
+++ Web-Development/Ruby/5- Loops & Iterators/15.rb
@@ -0,0 +1,26 @@
+# Looping with 'Until'
+# Good work!
+
+# i = 3
+# while i > 0 do
+#   print i
+#   i -= 1
+# end
+
+# j = 3
+# until j == 0 do
+#   print j
+#   j -= 1
+# end
+# In the example above, we wrote the same loop using while and using until.
+
+# Instructions
+# Now rewrite your while loop using until.
+
+# You still want to print out the numbers 1 through 50, inclusive.
+
+i = 1
+until i > 50 do
+    print i
+    i += 1
+end
