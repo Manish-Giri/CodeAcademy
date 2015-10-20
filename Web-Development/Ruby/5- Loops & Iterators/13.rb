a9d8980f0434a0acef9ecb55673a4fc20c1af2f1
Manish Giri <manish.giri.me@gmail.com>
2015-10-19 21:21:44 -0400

    #13 - .times - runs a block for the specified # times

---
+++ Web-Development/Ruby/5- Loops & Iterators/13.rb
@@ -0,0 +1,12 @@
+# The .times Iterator
+# The .times method is like a super compact for loop: it can perform a task on each item in an object a specified number of times.
+
+# For example, if we wanted to print out "Chunky bacon!" ten times, we might type
+
+# 10.times { print "Chunky bacon!" }
+# We don't know why we would type that, but we could.
+
+# Instructions
+# Use the .times operator to print out a string of your choice any number of times you like. Use the code in the text above as a guide if you need hel
+
+4.times {print "Hi Manish "}
