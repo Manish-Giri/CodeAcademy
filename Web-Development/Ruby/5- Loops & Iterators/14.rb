e1dc71ab30f2f6f4643131875e5e017c6d72af4b
Manish Giri <manish.giri.me@gmail.com>
2015-10-19 21:22:12 -0400

    #14 - using while loop

---
+++ Web-Development/Ruby/5- Loops & Iterators/14.rb
@@ -0,0 +1,20 @@
+# Looping with 'While'
+# Okay, training wheels off. Let's see your stuff!
+
+# i = 3
+# while i > 0 do
+#   print i
+#   i -= 1
+# end
+# In the above example, we create a variable called i and set it to 3.
+# Then, we print out 321 since we execute the loop so long as i is positive.
+# Instructions
+# Use a while loop to print out the numbers 1 through 50, inclusive. While the example above counts down, you will want to count up.
+
+# Use print rather than puts, and don't forget to increment your variable.
+
+i = 1
+while i <= 50 do
+    print i
+    i += 1
+end
