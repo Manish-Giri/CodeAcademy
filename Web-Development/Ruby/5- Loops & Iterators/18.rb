56fdc00ccb6df15aa0e260b067ddbb8d7cdf26a5
Manish Giri <manish.giri.me@gmail.com>
2015-10-20 20:35:45 -0400

    #18 - using .times

---
+++ Web-Development/Ruby/5- Loops & Iterators/18.rb
@@ -0,0 +1,7 @@
+# Iterating with .times
+# Finally, let's replace our loop with the .times iterator.
+
+# Instructions
+# Use the .times iterator to print "Ruby!" to the console 30 times. Hit Save & Submit Code to see the majesty of your code and complete this lesson!
+
+30.times {print "Ruby!"}
