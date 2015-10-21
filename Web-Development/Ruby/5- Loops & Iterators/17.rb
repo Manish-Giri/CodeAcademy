fb01bd2f6b97756f42482e091e95cef48b90af39
Manish Giri <manish.giri.me@gmail.com>
2015-10-20 20:35:21 -0400

    #17 - breaking out with break

---
+++ Web-Development/Ruby/5- Loops & Iterators/17.rb
@@ -0,0 +1,22 @@
+# Loop the Loop with Loop
+# Great work! We'll give you a bit of a break from the numbers game.
+
+# m= 0
+# loop do
+#     m += 1
+#     print m
+#     break if m == 10
+# end
+# In the example above, we print out 12345678910 since we loop 10 times.
+
+# Instructions
+# Use the loop iterator to print out the string "Ruby!" 30 times.
+
+# Make sure to type the string exactly as shown!
+
+i = 0
+loop do 
+    print "Ruby!"
+    i += 1
+    break if i == 30
+end
