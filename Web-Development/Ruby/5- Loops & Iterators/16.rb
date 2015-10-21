5062ec76710051b31f91617fb37a589cd3266fba
Manish Giri <manish.giri.me@gmail.com>
2015-10-20 20:34:59 -0400

    #16 - looping with for

---
+++ Web-Development/Ruby/5- Loops & Iterators/16.rb
@@ -0,0 +1,14 @@
+# Looping with 'For'
+# In case you're not picking up on the theme of Ruby having a gajillion ways to do any given task: let's convert our loop yet again.
+
+# for k in 1..3
+#   print k
+# end
+# In the above example, we print out 123 by virtue of looping from 1 to 3 inclusive.
+
+# Instructions
+# Now print out the numbers from 1 to 50 inclusive, using a for loop instead of an until loop.
+
+for i in 1..50
+print i
+end
