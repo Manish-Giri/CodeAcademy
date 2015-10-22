290f62a39e967d9df58c69f393343c25d43cf339
Manish Giri <manish.giri.me@gmail.com>
2015-10-22 19:43:10 -0400

    #2 - input with gets.chomp

---
+++ Web-Development/Ruby/6- Redacted/2.rb
@@ -0,0 +1,9 @@
+# # Getting the User's Input
+# # First things first: we'll need to get the user's input.
+
+# # Instructions
+# # Use puts to prompt the user for input two times. For the first puts, declare a variable called text and set it equal to the user's input via gets.chomp. For the second puts, declare a variable called redact and set it equal to the user's input using gets.chomp.
+puts "Enter text to search through"
+text = gets.chomp
+puts "Enter word to redact"
+redact = gets.chomp
