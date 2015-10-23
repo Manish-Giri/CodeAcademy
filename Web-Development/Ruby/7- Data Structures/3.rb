8d97a978d9ec5678743a283b89171396b653489f
Manish Giri <manish.giri.me@gmail.com>
2015-10-23 18:34:00 -0400

    #3 - An array of strings

---
+++ Web-Development/Ruby/7- Data Structures/3.rb
@@ -0,0 +1,7 @@
+# Arrays of Non-Numbers
+# Here's something you might not have known: you can make an array of any collection of Ruby objects. You can make an array of booleans! An array of strings! The list is (almost) endless.
+
+# Instructions
+# Create a new array called string_array. Make it an array of... strings!
+
+string_array = ["array", "of", "strings"]
