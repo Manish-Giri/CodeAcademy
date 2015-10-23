02bebf9afadf7e4eb1af79bfdee8b5cdfc2a9864
Manish Giri <manish.giri.me@gmail.com>
2015-10-23 18:33:18 -0400

    #1 - beginning of data structures with arrays

---
+++ Web-Development/Ruby/7- Data Structures/1.rb
@@ -0,0 +1,7 @@
+# Creating Arrays
+# Earlier we saw that an array can be used to store a list of values in a single variable. You can stuff any number of numbers in there, you can repeat numbers, and they don't have to be in numeric order!
+
+# Instructions
+# Declare a variable, my_array, in the editor, and set it equal to an array of your choice. Check the Hint if you need a syntax refresher.
+
+my_array = [10,2,33,400,5]
