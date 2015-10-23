1f853c185265858dab138b13a125d0c71eb420f8
Manish Giri <manish.giri.me@gmail.com>
2015-10-23 18:34:49 -0400

    #5 - 2-D arrays

---
+++ Web-Development/Ruby/7- Data Structures/5.rb
@@ -0,0 +1,7 @@
+# Create Your Own
+# See how a two-dimensional array with the same number of elements per row and overall rows is a square? An array (like a line) is one-dimensional; an array of arrays (like a square) is two-dimensional.
+
+# Instructions
+# Create your own two-dimensional array called my_2d_array in the editor. The elements of the inner array can be anything you like: numbers, strings, booleans, and so on. Check the Hint if you need help.
+
+my_2d_array = [[1,2],  [2,3], [3,4], [4,5]]
