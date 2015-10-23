27db93ade1c4c2fd5f5f660e0fda7bbf77d6bedc
Manish Giri <manish.giri.me@gmail.com>
2015-10-23 18:35:50 -0400

    #7 - creating an empty hash with Hash.new

---
+++ Web-Development/Ruby/7- Data Structures/7.rb
@@ -0,0 +1,12 @@
+# Using Hash.new
+# What we just showed you was hash literal notation. We call it that because you literally describe what you want in the hash: you give it a name and you set it equal to a bunch of key => value pairs inside curly braces.
+
+# You can also create a hash using Hash.new, like so:
+
+# my_hash = Hash.new
+# Setting a variable equal to Hash.new creates a new, empty hash; it's the same as setting the variable equal to empty curly braces ({}).
+
+# Instructions
+# Use Hash.new to create a new hash called pets. Hash must be capitalized, or Ruby won't know what you're talking about!
+
+pets = Hash.new
