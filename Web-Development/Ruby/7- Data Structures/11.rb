dff132b8cec8be3661df4a140bbca53ef09014cf
Manish Giri <manish.giri.me@gmail.com>
2015-10-24 19:20:59 -0400

    #11- iterating over arrays

---
+++ Web-Development/Ruby/7- Data Structures/11.rb
@@ -0,0 +1,23 @@
+# Iterating Over Arrays
+# Iterating over arrays is easier than it looks.
+
+# numbers = [1, 2, 3, 4, 5]
+# numbers.each { |element| puts element }
+# In the example above, we create an array called numbers with 5 elements.
+# Then we say, "Take this array and for each element, print it to the console." As usual, we can use any placeholder name for the bit between two | | characters.
+# Instructions
+# Use the .each iterator to puts out each element of the languages array.
+
+# Make sure to use puts instead of print, so each element is on its own line.
+
+languages = ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
+
+languages.each { |lang| puts lang }
+
+
+# HTML
+# CSS
+# JavaScript
+# Python
+# Ruby
+# ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
