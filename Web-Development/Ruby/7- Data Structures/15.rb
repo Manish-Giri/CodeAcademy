c27f890a1aa5627003ed97cadb1c01ccc3263147
Manish Giri <manish.giri.me@gmail.com>
2015-10-26 23:06:38 -0400

    #15 - creating a new hash

---
+++ Web-Development/Ruby/7- Data Structures/15.rb
@@ -0,0 +1,17 @@
+# Hashes
+# Good! Now let's create a hash. Feel free to use either hash literal notation or Hash.new.
+
+# prices = { 
+#   "apple" => 0.52,
+#   "banana" => 0.23,
+#   "kiwi" => 1.42
+# }
+
+# sounds = Hash.new
+# sounds["dog"] = "woof"
+# sounds["cat"] = "meow"
+# Instructions
+# Create a hash called my_hash in the editor.
+# Give it at least one key-value pair.
+
+my_hash = { "code" => "Rails"}
