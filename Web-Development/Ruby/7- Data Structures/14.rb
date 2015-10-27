b486d4029ddcab95f23f92f45e8c5248a58bd360
Manish Giri <manish.giri.me@gmail.com>
2015-10-26 23:06:16 -0400

    #14 - creating a multi-dimensional array

---
+++ Web-Development/Ruby/7- Data Structures/14.rb
@@ -0,0 +1,7 @@
+# Multidimensional Arrays
+# Great work! You've learned a lot in this lesson. Let's do a little review to be sure you really know your stuff.
+
+# Instructions
+# Create your own multidimensional array called my_array in the editor. The elements of the innermost array can be anything you like: numbers, strings, booleans, and so on. Check the Hint if you need help.
+
+my_array = [ [1,2], [3,4], [4,5],[5,6]]
