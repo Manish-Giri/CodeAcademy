52d7b2ed1c2bba35d05501cf47cac6966358818b
Manish Giri <manish.giri.me@gmail.com>
2015-10-27 21:10:28 -0400

    #2 - getting input with gets

---
+++ Web-Development/Ruby/8- Histogram/2.rb
@@ -0,0 +1,8 @@
+# You Know the Drill
+# You know by now how we've gotta start: we need to get input from the user.
+
+# Instructions
+# Use a puts statement to prompt the user for input. Use gets.chomp to save this input to a variable called text.
+
+puts "Enter text"
+text = gets.chomp
