0963be43f848fc8b358f27fef1bdb3daad53f96f
Manish Giri <manish.giri.me@gmail.com>
2015-10-28 20:09:55 -0400

    #3 - own methods

---
+++ Web-Development/Ruby/9- Methods, Blocks & Sorting/3.rb
@@ -0,0 +1,21 @@
+# Create Your Own
+# Now it's time for you to build your own method. Remember, the syntax looks like this:
+
+# def method_name
+#   # Do something!
+# end
+# Instructions
+# Create a method, greeting, in the editor. It should use puts to print a greeting to the console.
+
+# Define your method below!
+def greeting
+    puts "Greetings, Kid!"
+end
+
+
+
+
+# Define your method above this line.
+
+greeting # Ignore this for now. We'll explain
+         # it in the next exercise!
