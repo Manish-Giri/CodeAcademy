25e5bf063224028c966f4409516e8176d38161c0
Manish Giri <manish.giri.me@gmail.com>
2015-10-29 21:28:07 -0400

    while loop

---
+++ Web-Development/Python/Tutorials/Loops/while.py
@@ -0,0 +1,23 @@
+count = 0
+
+if count < 5:
+    print "Hello, I am an if statement and count is", count
+    
+while count < 10:
+    print "Hello, I am a while and count is", count
+    count += 1
+
+loop_condition = True
+
+while loop_condition:
+    print "I am a loop"
+    loop_condition = False
+
+num = 1
+
+while num < 11:  # Fill in the condition
+    # Print num squared
+    print num **2
+    
+    # Increment num (make sure to do this!)
+    num += 1
