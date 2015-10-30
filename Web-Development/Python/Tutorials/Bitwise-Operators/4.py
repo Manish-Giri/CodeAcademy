bd6a54d2f610da721c7885ff2a05bb33312e59b5
Manish Giri <manish.giri.me@gmail.com>
2015-10-30 17:53:28 -0400

    using bin() to print binary representation

---
+++ Web-Development/Python/Tutorials/Bitwise-Operators/4.py
@@ -0,0 +1,22 @@
+# The bin() Function
+# Excellent! The biggest hurdle you have to jump over in order to understand bitwise operators is learning how to count in base 2. Hopefully the lesson should be easier for you from here on out.
+
+# There are Python functions that can aid you with bitwise operations. In order to print a number in its binary representation, you can use the bin() function. bin() takes an integer as input and returns the binary representation of that integer in a string. (Keep in mind that after using the bin function, you can no longer operate on the value like a number.)
+
+# You can also represent numbers in base 8 and base 16 using the oct() and hex() functions. (We won't be dealing with those here, however.)
+
+# Instructions
+# We've provided an example of the bin function in the editor. Go ahead and use print and bin() to print out the binary representations of the numbers 2 through 5, each on its own line.
+
+print bin(1)
+print bin(2)
+print bin(3)
+print bin(4)
+print bin(5)
+
+# 0b1
+# 0b10
+# 0b11
+# 0b100
+# 0b101
+# None
