e4f5b1f4dc695532ebe81d12f7686ee01c3af857
Manish Giri <manish.giri.me@gmail.com>
2015-10-30 17:52:57 -0400

    binary number values

---
+++ Web-Development/Python/Tutorials/Bitwise-Operators/3.py
@@ -0,0 +1,36 @@
+# I Can Count to 1100!
+# All right! Time to practice counting in binary.
+
+# To make sure you've got the hang of it, fill out the rest of the numbers all the way up to twelve. Please do not use the str() method or any other outside functions.
+
+# Here are a few numbers that will be good to know going forward -
+
+# 2**0 = 1
+# 2**1 = 2
+# 2**2 = 4
+# 2**3 = 8
+# 2**4 = 16
+# 2**5 = 32
+# 2**6 = 64
+# 2**7 = 128
+# 2**8 = 256
+# 2**9 = 512
+# 2**10 = 1024
+# You may recognize these numbers. Do you have a 32 or 64 bit system? Does your computer have a 256GB hard drive? Computers think in binary!
+
+# Instructions
+# Fill out the rest of the numbers with their corresponding binary values up to twelve in the editor to the right, using the 0bxxx format.
+
+one = 0b1
+two = 0b10
+three = 0b11
+four = 0b100 
+five = 0b101
+six = 0b110
+seven = 0b111
+eight = 0b1000
+nine = 0b1001
+ten = 0b1010
+eleven = 0b1011
+twelve = 0b1100
+
