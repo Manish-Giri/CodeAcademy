f3cc8898c082d0d2143a49d470b3fe1d61ab9f68
Manish Giri <manish.giri.me@gmail.com>
2015-10-31 19:46:09 -0400

    NOT operator

---
+++ Web-Development/Python/Tutorials/Bitwise-Operators/10.py
@@ -0,0 +1,20 @@
+# See? This is NOT That Hard!
+# The bitwise NOT operator (~) just flips all of the bits in a single number. What this actually means to the computer is actually very complicated, so we're not going to get into it. Just know that mathematically, this is equivalent to adding one to the number and then making it negative.
+
+# And with that, you've seen all of the basic bitwise operators! We'll see what we can do with these in the next section.
+
+# Instructions
+# Click Save & Submit Code and observe what the console prints out.
+
+print ~1
+print ~2
+print ~3
+print ~42
+print ~123
+
+-2
+-3
+-4
+-43
+-124
+None
