b07bd5548edfaa91909da7f2b7e9803a93bddd87
Manish-Giri <manish.giri.me@gmail.com>
2015-11-05 22:32:58 -0500

    Class Syntax

---
+++ Python/Tutorials/Introduction-Classes/2.py
@@ -0,0 +1,12 @@
+# Class Syntax
+# A basic class consists only of the class keyword, the name of the class, and the class from which the new class inherits in parentheses. (We'll get to inheritance soon.) For now, our classes will inherit from the object class, like so:
+
+# class NewClass(object):
+    # # Class magic here
+# This gives them the powers and abilities of a Python object. By convention, user-defined Python class names start with a capital letter.
+
+# Instructions
+# Create a class called Animal in the editor. For now, in the body of your class, use the pass keyword. (pass doesn't do anything, but it's useful as a placeholder in areas of your code where Python expects an expression.
+
+class Animal(object):
+    pass
