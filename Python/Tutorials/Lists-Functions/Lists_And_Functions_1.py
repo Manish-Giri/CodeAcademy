52b25d3d98bd02f556a8e2e5796c3da75ec9de2d
Manish-Giri <manish.giri.me@gmail.com>
2015-11-09 21:27:12 -0500

    lists and functions

---
+++ Python/Tutorials/Lists-Functions/Lists_And_Functions_1.py
@@ -0,0 +1,15 @@
+n = [1, 3, 5]
+# Add your code below
+print n[1]
+
+n[1] *= 5
+print n[1]
+
+
+# Append the number 4 here
+n.append(4)
+print n
+
+# Remove the first item in the list here
+n.remove(1)
+print n
