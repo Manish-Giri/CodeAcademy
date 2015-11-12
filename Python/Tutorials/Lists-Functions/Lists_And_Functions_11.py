b6a818f94b7a855efb726aca705b68a54f17af8c
Manish Giri <manish.giri.me@gmail.com>
2015-11-11 19:36:54 -0500

    concatenating lists with a function

---
+++ Python/Tutorials/Lists-Functions/Lists_And_Functions_11.py
@@ -0,0 +1,21 @@
+# Using two lists as two arguments in a function
+# Using multiple lists in a function is no different from just using multiple arguments in a function!
+
+# a = [1, 2, 3]
+# b = [4, 5, 6]
+# print a + b
+# # prints [1, 2, 3, 4, 5, 6]
+# The example above is just a reminder of how to concatenate two lists.
+
+m = [1, 2, 3]
+n = [4, 5, 6]
+
+# Add your code here!
+def join_lists(x,y):
+    return x + y
+
+
+
+
+print join_lists(m, n)
+# You want this to print [1, 2, 3, 4, 5, 6]
