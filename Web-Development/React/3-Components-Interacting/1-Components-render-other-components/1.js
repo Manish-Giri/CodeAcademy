d2c2377f2cb3addddb7e2a9c2ad4169c93d30564
Manish Giri <manish.giri.me@gmail.com>
2016-09-17 17:08:03 -0400

    Component interaction summary

---
+++ Web-Development/React/3-Components-Interacting/1-Components-render-other-components/1.js
@@ -0,0 +1,7 @@
+// A React application can contain dozens, or even hundreds, of components.
+//
+// Each component might be small and relatively unremarkable on its own. When combined, however, they can form enormous, fantastically complex ecosystems of information.
+//
+// In other words, React apps are made out of components, but what makes React special isn't components themselves. What makes React special is the ways in which components interact.
+//
+// This unit is an introduction to components interacting.
