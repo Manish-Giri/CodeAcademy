14a791dffea329d71314932050995d2bc69bde15
MANISH-GIRI <manish.giri.me@gmail.com>
2016-09-18 15:33:09 -0400

    Components also interact with props

---
+++ Web-Development/React/3-Components-Interacting/2-This.Props/1.js
@@ -0,0 +1,9 @@
+// The name of this unit is "Components Interacting."
+//
+// In the last lesson, you learned one way that components can interact: a component can render another component.
+//
+// In this lesson, you will learn another way that components can interact: a component can pass information to another component.
+//
+// Information that gets passed from one component to another is known as "props."
+//
+// Click Next to enter props-land!
