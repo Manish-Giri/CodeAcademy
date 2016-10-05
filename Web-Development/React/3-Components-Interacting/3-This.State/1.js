0913694413d10315eebe60ad62652721c127bcbe
Manish Giri <manish.giri.me@gmail.com>
2016-10-05 18:18:01 -0400

    Dynamic Information with state

---
+++ Web-Development/React/3-Components-Interacting/3-This.State/1.js
@@ -0,0 +1,10 @@
+// Dynamic information is information that can change.
+
+// React components will often need dynamic information in order to render. For example, a component displaying a game's score will need to check the score before it can display it.
+
+// There are two ways for a component to get dynamic information: props and state. Besides props and state, everything in a component should always stay exactly the same.
+
+// You just spent a long lesson learning about props. Now it's time to learn about state. props and state are all that you need to set up an ecosystem of interacting React components.
+
+// Click Next to get started!
+
