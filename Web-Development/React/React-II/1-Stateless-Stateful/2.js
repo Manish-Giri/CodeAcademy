ac6802eb7c97e37ad9ccb1c16b1467de97e621fa
MANISH-GIRI <manish.giri.me@gmail.com>
2016-10-25 17:57:12 -0400

    Create module: Stateless Components Inherit From Stateful Components

---
+++ Web-Development/React/React-II/1-Stateless-Stateful/2.js
@@ -0,0 +1,9 @@
+// Let's learn our first programming pattern!
+//
+// In this lesson, we'll take a look at a simple version of a programming pattern. The following lessons will expand upon that lesson, and by the end, we'll have a programming pattern in its full complexity.
+//
+// Our programming pattern uses two React components: a stateful component, and a stateless component. "Stateful" describes any component that has a getInitialState function; "stateless" describes any component that does not.
+//
+// In our pattern, a stateful component passes its state down to a stateless component.
+//
+// Click Next to walk through an example!
