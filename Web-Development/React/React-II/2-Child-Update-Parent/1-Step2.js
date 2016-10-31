29760192321a8db0f9e9bba0367ee379ebd27e92
MANISH-GIRI <manish.giri.me@gmail.com>
2016-10-31 13:01:38 -0400

    Child Component Update Parent's State - Step 2

---
+++ Web-Development/React/React-II/2-Child-Update-Parent/1-Step2.js
@@ -0,0 +1,27 @@
+/**
+ * Created by manishgiri on 10/31/16.
+ */
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ChildClass = require('./ChildClass');
+
+var ParentClass = React.createClass({
+    getInitialState: function () {
+        return { totalClicks: 0 };
+    },
+
+    handleClick: function () {
+        var total = this.state.totalClicks;
+        this.setState(
+            { totalClicks: total + 1 }
+        );
+    },
+
+    // The stateful component class passes down
+    // handleClick to a stateless component class:
+    render: function () {
+        return (
+            <ChildClass onClick={this.handleClick} />
+        );
+    }
+});
