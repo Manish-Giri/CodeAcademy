db077dd22886f56a170b1eea9323d5d5ed5817dc
Manish Giri <manish.giri.me@gmail.com>
2016-11-01 00:12:52 -0400

    Pass right props to right siblings

---
+++ Web-Development/React/React-II/3-Child-Update-Siblings/3-Sibling.js
@@ -0,0 +1,20 @@
+/**
+ * Created by manishgiri on 10/31/2016.
+ */
+
+var React = require('react');
+
+var Sibling = React.createClass({
+    render: function () {
+
+        return (
+            <div>
+                <h1>Hey, my name is Frarthur!</h1>
+                <h2>Don't you think Frarthur is the prettiest name ever?</h2>
+                <h2>Sure am glad that my parents picked Frarthur!</h2>
+            </div>
+        );
+    }
+});
+
+module.exports = Sibling;
