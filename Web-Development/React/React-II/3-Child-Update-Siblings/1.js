4332d883d17fdd901ce06cc3d04ae5e5ca6a877c
Manish Giri <manish.giri.me@gmail.com>
2016-10-31 20:13:31 -0400

    A child component uses a sibling to update the parent

---
+++ Web-Development/React/React-II/3-Child-Update-Siblings/1.js
@@ -0,0 +1,13 @@
+/**
+ * Created by hi on 10/31/2016.
+ */
+/*
+Patterns within patterns within patterns!
+
+    In lesson 1, you learned your first React programming pattern: a stateful, parent component passes down a prop to a stateless, child component.
+
+    In lesson 2, you learned that lesson 1's pattern is actually part of a larger pattern: a stateful, parent component passes down an event handler to a stateless, child component. The child component then uses that event handler to update its parent's state.
+
+    In this lesson, we will expand the pattern one last time. A child component updates its parent's state, and the parent passes that state to a sibling component.
+
+An understanding of this final pattern will be very helpful in the wild, not to mention in the next React course. Click Next and we'll build an example!*/
