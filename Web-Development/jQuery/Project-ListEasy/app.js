b3bf5687e9034bdfad90651d4aa3650c1fff6133
Manish Giri <manish.giri.me@gmail.com>
2016-11-09 18:46:47 -0500

    jQuery for Project ListEasy

---
+++ Web-Development/jQuery/Project-ListEasy/app.js
@@ -0,0 +1,30 @@
+/**
+ * Created by manishgiri on 11/9/2016.
+ */
+var template = function(text) {
+    return '<p><input type="checkbox"><i class="glyphicon glyphicon-star"></i><span>' + text + '</span><i class="glyphicon glyphicon-remove"></i></p>';
+};
+
+var main = function() {
+    $('form').submit(function() {
+        var text = $("#todo").val();
+        var html = template(text);
+        $(".list").append(html);
+        //text = '';
+        $("#todo").val('');
+
+        return false;
+    });
+
+    $(document).on("click", ".glyphicon-star", function() {
+        $(this).toggleClass("active");
+    });
+
+    $(document).on("click",".glyphicon-remove" , function() {
+        //$(this).toggleClass("active");
+        $(this).parent().remove();
+    });
+
+};
+
+$(document).ready(main);
