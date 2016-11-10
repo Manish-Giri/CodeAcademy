5ed1b3d17fc36fcd17aee589261c3e2cd9394013
MANISH-GIRI <manish.giri.me@gmail.com>
2016-11-10 01:03:02 -0500

    jQuery for project Birdman

---
+++ Web-Development/jQuery/Project-Birdman/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by manishgiri on 11/9/16.
+ */
+var main = function() {
+    $("img").click(function() {
+        $(".dropdown-menu").toggle();
+    });
+};
+
+$(document).ready(main);
